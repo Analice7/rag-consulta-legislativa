b5e1335a8fa06b8172a0b80e099098439af651ff
Analice Silva <149388566+Analice7@users.noreply.github.com>
2025-02-09 14:58:40 -0300

    Remove processamento em batches

--- src/backend/embeddings.py
+++ src/backend/embeddings.py
@@ -3,11 +3,9 @@ import numpy as np
 from langchain.schema import Document
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
+from sklearn.preprocessing import normalize
 from dotenv import load_dotenv
 import config
-
-def normalize(vecs):
-    return vecs / np.linalg.norm(vecs, axis=1, keepdims=True)
 
 load_dotenv()
 
@@ -15,8 +13,7 @@ model = config.MODEL_EMBEDDINGS
 embeddings = HuggingFaceEmbeddings(model_name=model)
 print("Embeddings carregadas com sucesso.")
 
-batch_size = 500 
-docsdb = None 
+all_docs = []  
 
 for folder in ['atividade_legislativa', 'leis', 'vetos']:
     try:
@@ -26,30 +23,27 @@ for folder in ['atividade_legislativa', 'leis', 'vetos']:
         total_docs = len(chunks)
         print(f"Pasta {folder} contém {total_docs} documentos.")
 
-        for i in range(0, total_docs, batch_size):
-            batch = chunks[i : i + batch_size]
-
-            docs_batch = [
-                Document(page_content=chunk["chunk"], metadata=chunk["metadata"])
-                for chunk in batch
-            ]
-
-            if docsdb is None:
-                docsdb = FAISS.from_documents(docs_batch, embeddings)
-            else:
-                docsdb.add_documents(docs_batch)
-
-            print(f"Processados {min(i + batch_size, total_docs)}/{total_docs} documentos da pasta {folder}.")
+        all_docs.extend([
+            Document(page_content=chunk["chunk"], metadata=chunk["metadata"])
+            for chunk in chunks
+        ])
 
     except Exception as e:
         print(f"Erro ao processar {folder}: {e}")
 
-if docsdb:
+if all_docs:
+    print(f"Total de documentos carregados: {len(all_docs)}")
+    
+    embeddings_matrix = np.array(embeddings.embed_documents([doc.page_content for doc in all_docs]))
+    normalized_embeddings = normalize(embeddings_matrix, axis=1, norm='l2')
+
+    docsdb = FAISS.from_documents(all_docs, embeddings)
+
     docsdb.save_local("../../data/embeddings/all/")
     print("Índice FAISS criado e salvo com sucesso.")
 
-    # Teste de busca
     sample_doc = docsdb.similarity_search("teste", k=1)
     print(f"Resultado FAISS: {sample_doc[0].metadata if sample_doc else 'Nenhum resultado encontrado'}")
+
 else:
     print("Nenhum documento foi processado.")
